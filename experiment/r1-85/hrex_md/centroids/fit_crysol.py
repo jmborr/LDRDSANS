ab583149ee2217e44ce6268fd82b0c47d31cff12
Jose Borreguero <borreguero@gmail.com>
2017-11-05 17:30:54 -0500

    Refs #1Updated fits with optimal rho

--- experiment/r1-85/hrex_md/centroids/fit_crysol.py
+++ experiment/r1-85/hrex_md/centroids/fit_crysol.py
@@ -1,3 +1,9 @@
+"""For the topmost clusters in the hierarchical tree, fit their
+crysol-derived profiles to experiment.
+Also load radius of gyration for the centroids and propagate up
+the tree.
+"""
+
 from __future__ import print_function
 
 import os
@@ -14,7 +20,6 @@ tree.from_linkage_matrix(Z)
 # Load the SAXS profiles for each centroid and propagate up the tree nodes
 saxs_props = list()
 profdir = 'crysol_int'
-profdir = 'crysol_int'
 for index in open('centroids.names', 'r').readlines():
     # SAXS property
     file_name = os.path.join(profdir,'{}00.int'.format(index.strip()))
@@ -26,19 +31,19 @@ properties.propagator_size_weighted_sum(saxs_props, tree)
 
 # Load the Rg profiles for each centroid and propagate up the tree nodes
 rg_props = list()
-for rg in [float(r) for r in open('crysol_fit/shell_Rg.dat').readlines()]:
+for rg in np.loadtxt('crysol_fit/rg_chi2.dat', usecols=1):
     ps = properties.ScalarProperty(name='Rg')
     ps.setScalar(rg)
     rg_props.append(ps)
 properties.propagator_size_weighted_sum(rg_props, tree)
 
-# Fit to experimental profile
+# Load experimental profile to a SaxsProperty and fit the topmost clusters
 exp_prof = '../SAXS_SH4_UD.v2.dat'
 exp_prop = properties.SaxsProperty()
 exp_prop.from_ascii(exp_prof)
 fits_out = bayes.do_fit_at_depth_tree(tree, exp_prop, 'saxs', max_depth=10)
 
-# Output scaling parameters
+# Output scaling parameters and chi2
 for depth, fit_out in enumerate(fits_out):
     chi_squared = fit_out.OutputChi2overDoF
     scalings = list()
@@ -53,25 +58,32 @@ for depth, fit_out in enumerate(fits_out):
           ' '.join(['{:5.3f}'.format(s) for s in list(scalings)]))
 chis_squared = [fit_output.OutputChi2overDoF for fit_output in fits_out]
 print(' '.join('{:5.2f}'.format(c) for c in chis_squared))
+buffer = '# depth  Chi2\n'
+for i, chi2 in enumerate(chis_squared):
+    buffer += '{:3d} {:5.2f}\n'.format(i, chi2)
+open('chi2_vs_depth.dat', 'w').write(buffer)
 plt.plot(range(len(fits_out)), chis_squared)
-#plt.show()
+plt.show()
 
 """
-depth =  0 chi2 =29.21 scalings = 1.000
-depth =  1 chi2 =17.63 scalings = 1.000 0.000
-depth =  2 chi2 =19.99 scalings = 0.000 0.775 0.225
-depth =  3 chi2 =11.15 scalings = 0.644 0.027 0.000 0.329
-depth =  4 chi2 =11.69 scalings = 0.535 0.123 0.023 0.319 0.000
-depth =  5 chi2 =14.25 scalings = 0.422 0.000 0.054 0.175 0.075 0.274
-depth =  6 chi2 =14.68 scalings = 0.152 0.351 0.000 0.045 0.142 0.062 0.249
-depth =  7 chi2 =14.40 scalings = 0.147 0.126 0.230 0.308 0.000 0.038 0.104 0.048
-depth =  8 chi2 =13.41 scalings = 0.189 0.129 0.104 0.189 0.244 0.023 0.000 0.029 0.092
-depth =  9 chi2 =13.92 scalings = 0.096 0.173 0.114 0.082 0.094 0.168 0.223 0.022 0.000 0.028
-
+depth =  0 chi2 =19.38 scalings = 1.000
+depth =  1 chi2 = 9.05 scalings = 0.999 0.001
+depth =  2 chi2 =10.99 scalings = 0.000 0.787 0.213
+depth =  3 chi2 = 5.32 scalings = 0.613 0.036 -0.000 0.351
+depth =  4 chi2 = 5.52 scalings = 0.514 0.145 0.018 0.323 0.000
+depth =  5 chi2 = 6.96 scalings = 0.424 -0.000 0.043 0.190 0.065 0.278
+depth =  6 chi2 = 7.26 scalings = 0.147 0.354 0.000 0.036 0.154 0.053 0.256
+depth =  7 chi2 = 6.94 scalings = 0.153 0.120 0.237 0.314 0.000 0.029 0.109 0.038
+depth =  8 chi2 = 6.56 scalings = 0.182 0.140 0.100 0.189 0.242 0.019 0.000 0.023 0.105
+depth =  9 chi2 = 6.64 scalings = 0.106 0.172 0.118 0.084 0.090 0.172 0.224 0.013 0.000 0.021
 """
-#  Chi2 values: 29.21 17.63 19.99 11.15 11.69 14.25 14.68 14.40 13.41 13.92
+# Chi2 values at increasing depths, starting with zero
+# 19.38  9.05 10.99  5.32  5.52  6.96  7.26  6.94  6.56  6.64
+
+# Output radius of gyration and cluster size for the clusters at the level
+# that minimizes chi2 from the previous fit.
 depth_optimal = 3  # minimizes chi2
 nodes = tree.clusters_at_depth(depth_optimal)
-print(' '.join('{:5.2f}'.format(node['Rg'].y) for node in nodes ))  # 26.66 22.66 20.70 28.53
-tr()
-print(' '.join('{:3d}'.format(node.count) for node in nodes ))  # 
+print(' '.join('{:5.2f}'.format(node['Rg'].y) for node in nodes ))  #
+
+print(' '.join('{:3d}'.format(node.count) for node in nodes ))  # 22 270 330 378
--- experiment/r1-85/hrex_md/centroids/fit_crysol_int.py
+++ experiment/r1-85/hrex_md/centroids/fit_crysol_int.py
@@ -1,3 +1,6 @@
+"""For each centroid, do a fit of its crysol profile to experiment
+"""
+
 from __future__ import print_function
 
 import os
@@ -7,27 +10,30 @@ from idpflex import cnextend, properties, bayes
 import matplotlib.pyplot as plt
 from pdb import set_trace as tr
 
-# Fit to experimental profile
+# Load experimental profile to a SaxsProperty
 root_dir = '/home/jbq/repositories/LDRDSANS/experiment/r1-85/hrex_md'
 exp_prof = os.path.join(root_dir, 'SAXS_SH4_UD.v2.dat')
 exp_prop = properties.SaxsProperty()
 exp_prop.from_ascii(exp_prof)
 
-# Load the SAXS profiles for each centroid and fit
+# Load the SAXS profiles for each centroid and fit to experiment with Mantid
 chis_squared = list()
 scalings = list()
 backgrounds = list()
 centroid_indexes = [i.strip() for i in open('centroids.names', 'r').readlines()]
 for index in centroid_indexes:
     print('index =', index)
+    # Load the centroid's crysol profile to a SaxsProperty
     file_name = os.path.join('crysol_int', '{}00.int'.format(index))
     ps = properties.SaxsProperty(name='saxs')
-    ps.from_crysol(file_name)
+    ps.from_crysol_int(file_name)
     ps.y *= 1.0e-05  # Make computed and experimental intensities similar
     node = cnextend.ClusterNodeX(0)
     node.add_property(ps)
+    # Create the fit model with the centroid's profile and fit
     f = bayes.model_at_node(node, 'saxs')
     fit_output = bayes.do_fit(f, exp_prop)
+    # Retrieve chi2, scaling and background parameters
     chis_squared.append(fit_output.OutputChi2overDoF)
     p_table = fit_output.OutputParameters
     for irow in range(p_table.rowCount()):
@@ -38,12 +44,15 @@ for index in centroid_indexes:
             backgrounds.append(row['Value'])
     #SaveNexus(InputWorkspace=fit_output,
     #          Filename='/tmp/junk.nxs')
+
+# Save chi2, scaling and background to a file
 i_centroid_indexes = [int(i) for i in centroid_indexes]
 cxs = np.array([i_centroid_indexes, scalings, backgrounds, chis_squared])
 np.savetxt('fit_crysol_int.dat', cxs.transpose(),
            fmt='%05d           %7.3f         %7.3f            %5.2f',
            header='frame-number  scaling(1E-5)  background(1E-5)  chi_squared')
+# Scatter plot the the chi2 values
 plt.scatter(i_centroid_indexes, chis_squared)
 plt.show()
-print('bye')
+
 
